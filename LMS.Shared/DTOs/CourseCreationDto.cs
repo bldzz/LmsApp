c8e71291d4faa6cb5dd8645de9913034d1c0b9f3
Andreas Johansson <glanshammar70@gmail.com>
2025-01-09 16:12:10 +0100

    Added CourseCreationDto

---
+++ LMS.Shared/DTOs/CourseCreationDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LMS.Shared.DTOs;
+
+public record CourseCreationDto()
+{
+    [Required]
+    [MaxLength(255, ErrorMessage = "Course name cannot exceed 255 characters long.")]
+    [MinLength(3, ErrorMessage = "Course name must be at least 3 characters long.")]
+    public string CourseName { get; set; }
+    
+    [Required]
+    public DateTime StartDate { get; set; }
+    
+    [Required]
+    public DateTime EndDate { get; set; }
+}
