b6dc7622187ee87840d43215a96333cdec85b3df
Andreas Johansson <glanshammar70@gmail.com>
2025-01-09 14:20:04 +0100

    Added DTOs

---
+++ LMS.Shared/DTOs/DocumentDto.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LMS.Shared.DTOs;
+
+public record DocumentDto()
+{
+    [Required]
+    public string Id { get; set; }
+    
+    [Required]
+    public string UserId { get; set; }
+    
+    [Required]
+    public int? CourseId { get; set; }
+    
+    [Required]
+    public int? ModuleId { get; set; }
+    
+    [Required]
+    public Guid? ActivityId { get; set; }
+}
---
+++ LMS.Shared/DTOs/RoleDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LMS.Shared.DTOs;
+
+public record RoleDto()
+{
+    [Required]
+    [MaxLength(50)]
+    public string RoleName { get; set; }
+    
+    public List<string> UserIds { get; set; } = new List<string>();
+}
