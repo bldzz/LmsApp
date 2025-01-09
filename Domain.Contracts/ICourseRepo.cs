05ac0c08f4359bab087ee9a354a790b2c0557f3b
Andreas Johansson <glanshammar70@gmail.com>
2025-01-09 16:10:32 +0100

    Added CourseRepo

---
+++ Domain.Contracts/ICourseRepo.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Models.Entites;
+
+namespace Domain.Contracts;
+
+public interface ICourseRepo : IRepositoryBase<Course>
+{
+    /*
+    Task<IEnumerable<Course>> GetCoursesAsync();
+    Task<Course> GetCourseAsync(int id);
+    Task<bool> AnyCourseAsync(int id);
+    void AddCourse(Course course);
+    void RemoveCourse(Course course);
+    void UpdateCourse(Course course);
+    */
+}
---
+++ LMS.Infrastructure/Repositories/CourseRepo.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Contracts;
+using Domain.Models.Entites;
+
+namespace LMS.Infrastructure.Repositories;
+
+public class CourseRepo : RepositoryBase<Course>
+{
+    public CourseRepo(LmsContext context) : base(context)
+    {
+    }
+}
