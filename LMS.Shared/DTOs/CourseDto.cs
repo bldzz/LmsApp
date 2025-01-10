be3c908ca1b79f93e1cd5df525cb235c42d13297
bldzz <boldzz@hotmail.com>
2025-01-10 15:22:25 +0100

    Added CourseDto Pages

--- LMS.Shared/DTOs/CourseDto.cs
+++ LMS.Shared/DTOs/CourseDto.cs
@@ -10,7 +10,7 @@ public record CourseDto
     [Required]
     [MaxLength(255, ErrorMessage = "Course name cannot exceed 255 characters long.")]
     [MinLength(3, ErrorMessage = "Course name must be at least 3 characters long.")]
-    public string CourseName { get; set; }
+    public string CourseName { get; set; } = string.Empty;
     
     [Required]
     public DateTime StartDate { get; set; }
