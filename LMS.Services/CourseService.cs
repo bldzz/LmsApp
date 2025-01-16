0ddf7f1395c260e60635eb717d8ad41aa673a32c
didrikm <didrik.molinder@protonmail.com>
2025-01-16 11:43:15 +0100

    made CourseService an implementation of ServiceBase

--- LMS.Services/CourseService.cs
+++ LMS.Services/CourseService.cs
@@ -1,88 +1,46 @@
 ﻿using AutoMapper;
 using Domain.Contracts;
-using Microsoft.EntityFrameworkCore;
-
 using Domain.Models.Entites;
 using LMS.Shared.DTOs;
-using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.EntityFrameworkCore;
 
-namespace LMS.Services;
-public class CourseService : ICourseService
+namespace LMS.Services
 {
-    private readonly IMapper _mapper;
-    private readonly IUnitOfWork _uow;
-    
-    public CourseService(IMapper mapper, IUnitOfWork unitOfWork)
+    public class CourseService : ServiceBase<Course, CourseDto, CourseCreationDto>, ICourseService
     {
-        _mapper = mapper;
-        _uow = unitOfWork;
-    }
-    
-    public async Task<IEnumerable<CourseDto>> GetCourseAsync()
-    {
-        var courses =  await _uow.CourseRepo.FindAll().ToListAsync();
-        return _mapper.Map<IEnumerable<CourseDto>>(courses);
-    }
-
-    public async Task<CourseDto> GetCourseAsync(int id)
-    {
-        var course = await _uow.CourseRepo.GetCourseByIdAsync(id);
-        return _mapper.Map<CourseDto>(course);
-    }
-
-    public async Task<CourseDto> PostCourse(CourseCreationDto dto)
-    {
-        var course = _mapper.Map<Course>(dto);
-        _uow.CourseRepo.Create(course);
-        await _uow.CompleteASync();
-        var createdCourse = await _uow.CourseRepo.FindByCondition(c => c.Id == course.Id).SingleAsync();
-        var returnDto = _mapper.Map<CourseDto>(createdCourse);
-        return returnDto;
-    }
-
-    public async Task<CourseDto> PutCourse(int id, CourseDto dto)
-    {
-        if(id != dto.Id)
+        public CourseService(IMapper mapper, IUnitOfWork unitOfWork) : base(mapper, unitOfWork)
         {
-            throw new InvalidDataException();
         }
-        var existingCourse = await _uow.CourseRepo.FindByCondition(c => c.Id == id).SingleAsync();
-        if (existingCourse == null)  throw new InvalidDataException(); 
-        existingCourse.CourseName = dto.CourseName;
-        existingCourse.StartDate = dto.StartDate;
-        existingCourse.EndDate = dto.EndDate;
-        _uow.CourseRepo.Update(existingCourse);
-        await _uow.CompleteASync();
-        var updatedCourse = await _uow.CourseRepo.FindByCondition(c => c.Id != id).SingleAsync();
-        var returnDto = _mapper.Map<CourseDto>(updatedCourse);
-        return returnDto;
-    }
 
-    public async Task<CourseDto> DeleteCourse(int id)
-    {
-        var courseToDelete = await _uow.CourseRepo.FindByCondition(c => c.Id == id).SingleAsync();
-        if (courseToDelete == null) throw new InvalidDataException();
-        _uow.CourseRepo.Delete(courseToDelete);
-        await _uow.CompleteASync();
-        var shouldBeNull = await _uow.CourseRepo.FindByCondition(c => c.Id == id).SingleAsync();
-        if (shouldBeNull != null) throw new InvalidOperationException();
-        var placeholderDto = new CourseDto { Id = id, CourseName = "", StartDate = DateTime.Now, EndDate = DateTime.Now}; // TODO: fulhack, placeholder tills custom results är gjord
-        return placeholderDto;
-    }
+        protected override void CreateEntity(Course entity)
+        {
+            _uow.CourseRepo.Create(entity);
+        }
 
-    public async Task<CourseDto> PatchCourse(int id, JsonPatchDocument<CourseDto> patchDoc)
-    {
-        var existingCourse = await _uow.CourseRepo.FindByCondition(c => c.Id == id).SingleAsync();
-        if (existingCourse == null) throw new InvalidDataException();
+        protected override void DeleteEntity(Course entity)
+        {
+            _uow.CourseRepo.Delete(entity);
+        }
 
-        var courseDto = _mapper.Map<CourseDto>(existingCourse);
-        patchDoc.ApplyTo(courseDto);
+        protected override async Task<IEnumerable<Course>> GetAllEntitiesAsync()
+        {
+            return await _uow.CourseRepo.FindAll().ToListAsync();
+        }
 
-        _mapper.Map(courseDto, existingCourse);
-        _uow.CourseRepo.Update(existingCourse);
-        await _uow.CompleteASync();
+        protected override async Task<Course> GetEntityByIdAsync(int id)
+        {
+            return await _uow.CourseRepo.FindByCondition(m => m.Id == id).SingleAsync();
+        }
 
-        var updatedCourse = await _uow.CourseRepo.FindByCondition(c => c.Id == id).SingleAsync();
-        return _mapper.Map<CourseDto>(updatedCourse);
+        protected override void UpdateEntity(Course entity)
+        {
+            _uow.CourseRepo.Update(entity);
+        }
+
+        protected override async Task<bool> ValidateEntityExistsAsync(int id)
+        {
+            var entity = await _uow.CourseRepo.FindByCondition(m => m.Id == id).SingleOrDefaultAsync();
+            return entity != null;
+        }
     }
 }
