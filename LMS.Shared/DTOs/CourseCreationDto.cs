b500d38578c9c843298f2b77269b9d71796b5e7e
Mikael Pros <Mikael_Pros@hotmail.com>
2025-01-23 12:08:05 +0100

    Add blazor components for modals

--- LMS.Shared/DTOs/CourseCreationDto.cs
+++ LMS.Shared/DTOs/CourseCreationDto.cs
@@ -7,12 +7,12 @@ public record CourseCreationDto()
     [Required]
     [MaxLength(255, ErrorMessage = "Course name cannot exceed 255 characters long.")]
     [MinLength(3, ErrorMessage = "Course name must be at least 3 characters long.")]
-    public string CourseName { get; init; }
+    public string CourseName { get; set; }
     
     [Required]
-    public DateTime StartDate { get; init; }
+    public DateTime StartDate { get; set; }
     
     [Required]
-    public DateTime EndDate { get; init; }
-    public string Description { get; init; }
+    public DateTime EndDate { get; set; }
+    public string Description { get; set; }
 }
