d8793b39809b06f010e9ab5742ee49a1533f0060
Andreas Johansson <glanshammar70@gmail.com>
2025-01-24 06:40:03 +0100

    Added DTO for file upload

---
+++ LMS.Shared/DTOs/FileUploadDto.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
+
+namespace LMS.Shared.DTOs;
+
+public record FileUploadDto()
+{
+    [Required]
+    public string FileName { get; set; } = string.Empty;
+    
+    [Required]
+    public string Description { get; set; } = string.Empty;
+    
+    [Required]
+    public string UserId { get; set; } = string.Empty;
+    
+    [Required]
+    public IFormFile File { get; set; }
+    public string ContentType { get; set; } = "application/octet-stream";
+    public Stream FileStream { get; set; } = default!;
+}
